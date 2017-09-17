eebb9373f8b738ee77757a65aef70548d1769fdd
alper demirci <alper.demirci@gmail.com>
2017-09-17 13:25:02 -0400

    Added extention to fix the http secure connection issue that apple has been strically stuborn about it

--- WSJ RSS feed/XMLParserDataAdaptor.swift
+++ WSJ RSS feed/XMLParserDataAdaptor.swift
@@ -111,3 +111,12 @@ class XMLParserDataAdaptor: NSObject, XMLParserDelegate {
         print(parseError.localizedDescription)
     }
 }
+
+
+extension String
+{
+    func replace(target: String, withString: String) -> String
+    {
+        return self.replacingOccurrences(of: target, with: withString, options: NSString.CompareOptions.literal, range: nil)
+    }
+}
